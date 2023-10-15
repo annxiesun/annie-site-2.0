c4a48212e6398c58f6aa387a7e5c3769dc954325
annxiesun <anniesun@Annies-MacBook-Pro-2.local>
2023-10-14 21:20:39 -0400

    preload

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,24 +4,26 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {" "}
         <link
           rel="preload"
           href="assets/fonts/BRHendrix-Black.ttf"
           as="font"
           type="font/ttf"
+          crossOrigin="anonymous"
         />
         <link
           rel="preload"
           href="assets/fonts/BRHendrix-Regular.ttf"
           as="font"
           type="font/ttf"
+          crossOrigin="anonymous"
         />
         <link
           rel="preload"
           href="assets/fonts/BRHendrix-SemiBold.ttf"
           as="font"
           type="font/ttf"
+          crossOrigin="anonymous"
         />
       </Head>
       <body>
